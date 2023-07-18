9b54081eeec67dd83fcbf44c47d1b54d7f2ea5f9
Vyacheslav Khudyakov <slavak521@MacBook-Air-Vyacheslav.local>
2023-07-18 13:25:53 +0600

    Task complited

--- src/App.js
+++ src/App.js
@@ -1,53 +1,139 @@
 import "devextreme/dist/css/dx.dark.css";
 import "./App.css";
-import Button from "devextreme-react/button";
-import TextBox from "devextreme-react/text-box";
-import { useCallback, useState, useRef } from "react";
 
-let textValue = "";
+import DropDownBox from "devextreme-react/drop-down-box";
+import DataGrid, {
+  FilterRow,
+  Paging,
+  Scrolling,
+  Selection,
+} from "devextreme-react/data-grid";
+import ArrayStore from "devextreme/data/array_store";
+import { useReducer } from "react";
+
+import { customers } from "./customers";
+
+const initialState = {
+  gridBoxValue: [1],
+  selectedValue: [1],
+  isGridBoxOpened: false,
+};
+const store = new ArrayStore({
+  key: "ID",
+  data: customers,
+});
+
+const gridColumns = ["CompanyName", "City", "Phone"];
+const ownerLabel = { "aria-label": "Owner" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "toggle_dropdownbox_state": {
+      return {
+        ...state,
+        isGridBoxOpened: !state.isGridBoxOpened,
+      };
+    }
+
+    case "change_gridbox_value": {
+      return {
+        ...state,
+        gridBoxValue: action.newValue,
+        isGridBoxOpened: false,
+      };
+    }
+
+    case "grid_selection_change": {
+      return {
+        ...state,
+        selectedValue: action.newValue,
+      };
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
 
 function App() {
-  const [content, setContent] = useState(false);
-  const [value, setValue] = useState("");
-  const inputRef = useRef(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleValueChanged = useCallback((v) => {
-    setValue(v.value);
-  }, []);
+  const dataGridOnSelectionChanged = (e) => {
+    dispatch({
+      type: "grid_selection_change",
+      newValue: e.currentSelectedRowKeys,
+    });
+  };
 
-  const onClick = () => {
-    textValue = value;
-    setContent(!content);
+  const gridBoxDisplayExpr = (item) => {
+    return item && `${item.CompanyName} <${item.Phone}>`;
+  };
+
+  const syncDataGridSelection = (e) => {
+    dispatch({
+      type: "change_gridbox_value",
+      newValue: e.value,
+    });
+  };
+
+  const onGridBoxOpened = (e) => {
+    if (e.name === "opened") {
+      dispatch({ type: "toggle_dropdownbox_state" });
+    }
+  };
+
+  const enterKeyHandler = (e) => {
+    if (e.event && e.event.key === "Enter") {
+      dispatch({
+        type: "change_gridbox_value",
+        newValue: state.selectedValue,
+      });
+    }
+  };
+
+  const keyboardSelectionHandler = (e) => {
+    dispatch({
+      type: "grid_selection_change",
+      newValue: [e.rowIndex + 1],
+    });
+  };
+
+  const dataGridRender = (state) => {
+    return (
+      <DataGrid
+        dataSource={store}
+        columns={gridColumns}
+        hoverStateEnabled={true}
+        selectedRowKeys={state.gridBoxValue}
+        onSelectionChanged={dataGridOnSelectionChanged}
+        height="100%"
+        focusedRowEnabled={true}
+        onKeyDown={enterKeyHandler}
+        onFocusedRowChanged={keyboardSelectionHandler}
+      >
+        <Selection mode="single" />
+        <Scrolling mode="virtual" />
+        <Paging enabled={true} pageSize={10} />
+        <FilterRow visible={true} />
+      </DataGrid>
+    );
   };
 
   return (
     <div className="App">
-      <Button text="Click me!" type="success" onClick={onClick} />
-      <TextBox
-        className="textbox"
-        value={value}
-        onValueChanged={handleValueChanged}
-        label="Link"
-        labelMode="floating"
-      />
-      <TextBox
-        className="textbox"
-        ref={inputRef}
-        defaultValue=""
-        label="Link"
-        labelMode="floating"
-      />
-      {content && <TextBoxesContent inputRef={inputRef} value={textValue} />}
-    </div>
-  );
-}
-
-function TextBoxesContent(props) {
-  const { value, inputRef } = props;
-  return (
-    <div className="text-boxes-value">
-      <p>{value}</p>
-      <p>{inputRef.current.instance.option("value")}</p>
+      <DropDownBox
+        value={state.gridBoxValue}
+        opened={state.isGridBoxOpened}
+        valueExpr="ID"
+        inputAttr={ownerLabel}
+        displayExpr={gridBoxDisplayExpr}
+        placeholder="Select a value..."
+        showClearButton={true}
+        dataSource={store}
+        onValueChanged={syncDataGridSelection}
+        onOptionChanged={onGridBoxOpened}
+        contentRender={() => dataGridRender(state)}
+      ></DropDownBox>
     </div>
   );
 }
